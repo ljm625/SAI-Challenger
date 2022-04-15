bc7575370384836164a7abcc0121760232a6d35f
Jiaming Li <ljm625@gmail.com>
2022-04-15 17:18:56 +0800

    fix

--- common/sai-replayer.py
+++ common/sai-replayer.py
@@ -27,10 +27,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--rec-file", type=str, help="Path of sairedis.rec", required=True)
     parser.add_argument("-c", "--continue-with-swss", help="skip restart syncd, useful when using with swss", action="store_true")
-    parser.add_argument("-s", "--sync-mode", help="Enable gSync", action="store_true")
+    parser.add_argument("-s", "--sync-mode", help="Enable gSync (We do not support)", action="store_true")
     args = parser.parse_args()
-    import pydevd_pycharm
-    pydevd_pycharm.settrace('10.79.96.73', port=7999, stdoutToServer=True, stderrToServer=True)
     recording_file = args.rec_file
     sai = SaiNpu(exec_params)
     records = __parse_rec(recording_file)
@@ -43,7 +41,7 @@ if __name__ == '__main__':
         full_record = True
     if args.sync_mode:
         for key,record in records.items():
-            if "SAI_OBJECT_TYPE_SWITCH" in record[1]:
+            if record[0] == "c" and "SAI_OBJECT_TYPE_SWITCH" in record[1]:
                 for attr in record[2:]:
                     if "SAI_REDIS_SWITCH_ATTR_SYNC_MODE" in attr:
                         indx = record.index(attr)
